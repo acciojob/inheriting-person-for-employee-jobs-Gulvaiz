a4a97e8021a128ac566f671caeaf46ef0d417f21
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-09-20 00:06:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-function Person(name, age) {
-      this.name = name
-      this.age = age
- 
- }
- 
- Person.prototype.greet = function(){
-             console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)      
- }
- 
- function Employee(name, age, jobTitle) {
-       Person.call(this,name,age)
-       this.jobTitle = jobTitle
- }
+class Person {
+	constructor(name, age) {
+	    this.name = name;
+		this.age = age;
+	}
+		greet(){
+              console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+		}
+}
 
- Employee.prototype = Object.create(Person.prototype);
-
-Employee.prototype.constructor = Employee;
-
-Employee.prototype.jobGreet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
+class Employee {
+	constructor(name,age,jobTitle){
+		 Employee extends Person
+		this.jobTitle = jobTitle;
+	}
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+	}
+}
 
 
  // Do not change code below this line
